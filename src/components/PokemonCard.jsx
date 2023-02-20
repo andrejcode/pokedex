210f683e392ee87080856ea4bf1a7a3398cdabcf
Andrej <amilanovi0@gmail.com>
2023-02-20 05:13:33 +0100

    Implement modal dialog

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 const PokemonCard = ({ url }) => {
   const [pokemon, setPokemon] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -18,7 +26,41 @@ const PokemonCard = ({ url }) => {
   }, [url]);
 
   return (
-    <main>
+    <main onClick={toggleModal}>
+      {pokemon && (
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={toggleModal}
+          contentLabel="Pokemon Details"
+          className="modal"
+          overlayClassName="overlay"
+        >
+          <h1>
+            {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+          </h1>
+          <div>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <div>
+              <p>Height: {pokemon.height * 10} cm</p>
+              <p>Weight: {pokemon.weight / 10} kg</p>
+              <div>
+                <p>Type:</p>
+                <div className="modal-types">
+                  {pokemon.types.map((item) => (
+                    <p
+                      key={item.type.name}
+                      className={`type ${item.type.name}`}
+                    >
+                      {item.type.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
+
       {pokemon && (
         <div className="pokemon-card">
           <img src={pokemon.sprites.front_default} alt={pokemon.name} />
